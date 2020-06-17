a79d3bdbe929a78c8ee2222492772075aabfd1b0
EmuDevs <mrslinkx8@gmail.com>
2020-06-17 17:42:37 +0300

    Add files via upload

---
+++ Black Desert Private Launcher/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Black_Desert_Private_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.checkBoxRememberMe = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxServerIP = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Login = new System.Windows.Forms.Button();
+            this.textBoxUserEmail = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxServerPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(696, 346);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(140, 64);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(208, 20);
+            this.textBox_Password.TabIndex = 2;
+            this.textBox_Password.Text = "Password";
+            this.textBox_Password.UseSystemPasswordChar = true;
+            // 
+            // checkBoxRememberMe
+            // 
+            this.checkBoxRememberMe.AutoSize = true;
+            this.checkBoxRememberMe.Location = new System.Drawing.Point(140, 109);
+            this.checkBoxRememberMe.Name = "checkBoxRememberMe";
+            this.checkBoxRememberMe.Size = new System.Drawing.Size(94, 17);
+            this.checkBoxRememberMe.TabIndex = 3;
+            this.checkBoxRememberMe.Text = "Remember me";
+            this.checkBoxRememberMe.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Server IP";
+            // 
+            // textBoxServerIP
+            // 
+            this.textBoxServerIP.Location = new System.Drawing.Point(111, 35);
+            this.textBoxServerIP.Name = "textBoxServerIP";
+            this.textBoxServerIP.Size = new System.Drawing.Size(102, 20);
+            this.textBoxServerIP.TabIndex = 5;
+            this.textBoxServerIP.Text = "127.0.0.1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Login);
+            this.groupBox1.Controls.Add(this.textBoxUserEmail);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_Password);
+            this.groupBox1.Controls.Add(this.checkBoxRememberMe);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(31, 352);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 152);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(273, 105);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxUserEmail
+            // 
+            this.textBoxUserEmail.Location = new System.Drawing.Point(101, 35);
+            this.textBoxUserEmail.Name = "textBoxUserEmail";
+            this.textBoxUserEmail.Size = new System.Drawing.Size(247, 20);
+            this.textBoxUserEmail.TabIndex = 1;
+            this.textBoxUserEmail.Text = "Mail@wateva.com";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxServerPort);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxServerIP);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(436, 352);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 152);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Server";
+            // 
+            // textBoxServerPort
+            // 
+            this.textBoxServerPort.Location = new System.Drawing.Point(111, 66);
+            this.textBoxServerPort.Name = "textBoxServerPort";
+            this.textBoxServerPort.Size = new System.Drawing.Size(102, 20);
+            this.textBoxServerPort.TabIndex = 6;
+            this.textBoxServerPort.Text = "8888";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 515);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.CheckBox checkBoxRememberMe;
+        private System.Windows.Forms.TextBox textBoxServerIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxServerPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxUserEmail;
+        private System.Windows.Forms.Button Login;
+    }
+}
+
---
+++ Black Desert Private Launcher/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml.Xsl;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Black_Desert_Private_Launcher
+{
+    public partial class Form1 : Form
+    {
+        bool emailIsEmail = false; // used later to check if username is a valid email or not
+        public Form1()
+        {
+           /*******************************************************************
+            * Initialize the form, check if gameclient exists and get ready to go
+            *******************************************************************/
+            InitializeComponent();                                                   
+            this.Text = "Black Desert Online Private Launcher!";                    // Set Form's name
+            string currentdir = System.IO.Directory.GetCurrentDirectory();          // get the directory the launcher is run from
+            string exeFile = currentdir + "\\bin64\\BlackDesert64.exe";             // look for file under the current dir
+            bool ready0 = System.IO.File.Exists(exeFile);                           // check if game client exists returning true or false
+            if (ready0 == false)
+            {                                                                       // If returned false show error meassages and exit
+                MessageBox.Show(" Black Desert Online is missing! \n Make sure the launcher is in the same folder as the 'bin64' folder.", "Important! Game files are missing!");
+                Environment.Exit(0);
+            }                                                                       // if returned true continue setup
+
+            if (Properties.Settings.Default.Email != string.Empty)                  // check settings, if they exist fill in textboxes with previous data
+            {
+                textBoxUserEmail.Text = Properties.Settings.Default.Email;          // email
+                textBox_Password.Text = Properties.Settings.Default.Password;       // password
+                textBoxServerIP.Text = Properties.Settings.Default.serverIP;        // server ip
+                textBoxServerPort.Text = Properties.Settings.Default.serverPort;    // server port
+                checkBoxRememberMe.Checked = true;                                  // tick "remember me"
+            } else
+            {
+                textBoxUserEmail.Text = "Mail@wateva.com";                          // if settings dont exist fill textboxes with the default data
+                textBox_Password.Text = "Password";
+                textBoxServerIP.Text = "127.0.0.1";
+                textBoxServerPort.Text = "8888";
+                checkBoxRememberMe.Checked = false;                                 // untick "remember me"
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*******************************************************************
+             * Setup and then start the game client
+             *******************************************************************/
+            if (checkBoxRememberMe.Checked)                                         // check if remember me is checked
+            {
+                Properties.Settings.Default.Email = textBoxUserEmail.Text;          // if it is save these details to the computer for next launch
+                Properties.Settings.Default.Password = textBox_Password.Text;
+                Properties.Settings.Default.serverIP = textBoxServerIP.Text;
+                Properties.Settings.Default.serverPort = textBoxServerPort.Text;
+                Properties.Settings.Default.Save();                                 
+                checkBoxRememberMe.Checked = true;
+            }
+            Properties.Settings.Default.serverIP = textBoxServerIP.Text;            // get server ip from textbox to pass to clientRun()
+            Properties.Settings.Default.serverPort = textBoxServerPort.Text;        // same but for port
+
+            checkIsEmail();                                                         // check if email counts as a valid email address
+
+            if (emailIsEmail)                                                       // if it is create/update service.ini and run the game
+            {                                                                       // if its not it wont do anything else
+                serviceIni();
+                clientRun();
+            }
+        }
+
+        private void clientRun()
+        {
+            /*******************************************************************
+             * Setup a command prompt and pass the commands to it
+             *******************************************************************/
+            Process p = new Process();                                                  // Setup Process variable
+            ProcessStartInfo info = new ProcessStartInfo();                             // setup start info for the process
+            info.FileName = "cmd.exe";                                                  // file to run - in this case "cmd.exe"
+            info.RedirectStandardInput = true;                                          // so the "cmd.exe" can accept the StreamWriter from below
+            info.UseShellExecute = false;                                               // dont use the operating system shell to start the process.
+            info.CreateNoWindow = true;                                                 // hides the command prompt window
+            
+            p.StartInfo = info;
+            p.Start();                                                                  // start cmd.exe
+
+            using (StreamWriter sw = p.StandardInput)                                   // pass on these commands
+            {
+                if (sw.BaseStream.CanWrite)                                             // checks if able to write to cmd.exe before trying
+                {                                                                       // if returns true pass on commands
+                    sw.WriteLine("cd bin64");                                           // everything inside the "" is printed to the cmd.exe - one for each line
+                    sw.WriteLine("start BlackDesert64.exe" + " " + textBoxUserEmail.Text + "," + textBox_Password.Text);
+                }
+            }
+        }
+
+        private void serviceIni() // builds the servce.ini file
+        {
+
+    var MyIni = new IniFile("service.ini");
+
+            /*******************************************************************
+             *  if TYPE is not set under the section [SERVICE]
+             *  create it and set it to NA like this
+             *  
+             *  [SERVICE]
+             *  TYPE=NA
+             *******************************************************************/
+
+            if (!MyIni.KeyExists("TYPE", "SERVICE"))            // check if key exists
+            {
+                MyIni.Write("TYPE", "NA", "SERVICE");           // if it doesnt add it and set it
+            }
+            if (!MyIni.KeyExists("RES", "SERVICE"))             // repeat
+            {
+                MyIni.Write("RES", "_EN_", "SERVICE");
+            }
+            if (!MyIni.KeyExists("nationType", "SERVICE"))
+            {
+                MyIni.Write("nationType", "1", "SERVICE");
+            }
+            if (!MyIni.KeyExists("damageMeter", "SERVICE"))
+            {
+                MyIni.Write("damageMeter", "1", "SERVICE");
+            }
+            if (!MyIni.KeyExists("AUTHENTIC_DOMAIN", "NA"))
+            {
+                MyIni.Write("AUTHENTIC_DOMAIN", "127.0.0.1", "NA"); 
+            }
+            if (!MyIni.KeyExists("AUTHENTIC_PORT", "NA"))
+            {
+                MyIni.Write("AUTHENTIC_PORT", "8888", "NA");
+            }
+            if (!MyIni.KeyExists("PATCH_URL", "NA"))
+            {
+                MyIni.Write("PATCH_URL", "http://downtest1.black.game.daumserver.com/patch/", "NA");
+            }
+            
+            MyIni.Write("AUTHENTIC_DOMAIN", textBoxServerIP.Text, "NA"); // If server ip/port keys do exist in the file override them with the ip/port from the textboxes
+            MyIni.Write("AUTHENTIC_PORT", textBoxServerPort.Text, "NA"); // same as above
+        }
+
+        private void checkIsEmail()
+        {
+            /*******************************************************************
+             * Simple Regex verification that check's if the text entered in 
+             * the variable "textBoxUserEmail.Text" has atleast a "@" and a "."
+             *******************************************************************/
+                                                            // setup Regex dump to check against
+            Regex emailRegex = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+            if (emailRegex.IsMatch(textBoxUserEmail.Text))  // checks if input is infact an email address
+            {                
+                emailIsEmail = true;                        // it matches so set variable to true
+            }
+            else                                            // does not match so show error instead
+            {
+                DialogResult result = MessageBox.Show(" Username needs to be a valid email address.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+
+}
